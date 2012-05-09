c17260644b379544de10c19ecd93d91d3082fae2
Damien Flament <damien.flament@hotmail.fr>
2012-05-09 19:46:53 +0200

    c'est chiant

---
+++ Test.java
@@ -0,0 +1,34 @@
+
+
+public class Test{
+
+  public static void main(String []args){
+    System.out.println("");
+
+  }
+
+  public static void initialize(){
+
+  }
+
+  public static void destroy(){
+
+
+  }
+
+  public static void conflit(){
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
